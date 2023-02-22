01586a2baa7dbb731777bea16531b3247f8f4a52
Mahmoud Mohamed <mahmoudm.abdelwadood@gmail.com>
2023-02-22 19:46:27 +0200

    start using room database

--- app/src/main/java/com/mahmoud/roomtutorial/MainActivity.java
+++ app/src/main/java/com/mahmoud/roomtutorial/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PostsDatabase database = PostsDatabase.getInstance(this);
+        database.postsDao().inserPost(new Post(2, "Mahmoud", "Room Database Studying"));
+
     }
 }
