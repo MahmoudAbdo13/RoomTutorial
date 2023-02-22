271c768e6fc365645a0e6d87499a932e5f3eeb5c
Mahmoud Mohamed <mahmoudm.abdelwadood@gmail.com>
2023-02-22 19:44:21 +0200

    create room database

---
+++ app/src/main/java/com/mahmoud/roomtutorial/PostsDatabase.java
@@ -0,0 +1,24 @@
+package com.mahmoud.roomtutorial;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = Post.class, version = 1, exportSchema = false)
+public abstract class PostsDatabase extends RoomDatabase {
+
+    private static PostsDatabase instance;
+    public abstract PostsDao postsDao();
+
+    public static synchronized PostsDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(), PostsDatabase.class, "posts_db")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+
+        return instance;
+    }
+}
